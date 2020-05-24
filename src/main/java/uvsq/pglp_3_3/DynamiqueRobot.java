7ddd43ae62ff7cb6fff2dab2effc1863e833f48a
amine <amine.amirouche96@hotmail.com>
2020-05-24 19:35:22 +0200

    ajout de la classe DynamiqueRobot

---
+++ src/main/java/uvsq/pglp_3_3/DynamiqueRobot.java
@@ -0,0 +1,20 @@
+package uvsq.pglp_3_3;
+
+public class DynamiqueRobot extends Robot {
+	  /**
+	   * constructeur du robot dynamique.
+	   * @param position : la position actuelle du robot
+	   * @param orientation : l'orientation actuelle du robot
+	   */
+	  public DynamiqueRobot(int position, int orientation) {
+	    position = position;
+	    orientation = orientation % 4;
+	  }
+
+	  /**
+	   * methode qui fait avancer le robot.
+	   */
+	  public void avance() {
+	    position++;
+	  }
+	}
